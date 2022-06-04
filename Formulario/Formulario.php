410bb26f1da4ee76d5b57cbaaedd56a875cf97c1
Jeronimo Pourtau <101447160+Jeronit1@users.noreply.github.com>
2022-06-04 15:49:16 -0300

    Add files via upload

---
+++ Formulario/Formulario.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+    <title>Formulario</title>
+    <link rel="stylesheet" href="Style.css">
+</head>
+
+<body>
+    <?php
+    if (isset($_POST['submit'])) {
+        $name = trim($_POST['name']);
+        $age = trim($_POST['age']);
+        $email = trim($_POST['email']);
+        $tel = ($_POST['tel']);
+        $FechaRegistro = date("y/m/d");
+    }
+    ?>
+    <form action="" method="post">
+        <p>Nombre:<input type="text" name="name" value="<?php
+                                                        if (isset($name)) echo "$name" ?>" /></p>
+        <p>Edad:<input type="number" min=6 name="age" value="<?php
+                                                                if (isset($age)) echo "$age" ?>" /></p>
+        <p>Email:<input type="text" name="email" value="<?php
+                                                        if (isset($email)) echo "$email" ?>" /></p>
+        <p>Telefono<input type="number" name="tel" value="<?php
+                                                            if (isset($tel)) echo "$tel" ?>" /></p>
+        <p><input type="submit" name="submit" value="Enviar" /></p>
+    </form>
+    <?php
+    include("Server-Form.php");
+    ?>
+</body>
+
+</html>
---
+++ Formulario/Server-Form.php
@@ -0,0 +1,31 @@
+<link rel="stylesheet" href="Style.css">
+<?php
+include("Union-Server.php");
+if (isset($_POST['submit'])) {
+    if (empty($name)) {
+        echo "<p class='error'>* Agregue su Nombre</p>";
+    }
+    if (empty($age)) {
+        echo "<p class='error'>* Agregue su Edad</p>";
+    }
+    if (empty($email)) {
+        echo "<p class='error'>* Agregue su Email</p>";
+    } else {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "<p class='error'>* Su correo es incorrecto</p>";
+        }
+    }
+    if (empty($tel)) {
+        echo "<p class='error'>* Agregue su Telefono</p>";
+    }
+
+    if (strlen($_POST['name']) > 1 && strlen($_POST['age']) > 1 && strlen($_POST['email']) > 1 && strlen($_POST['tel']) > 1) {
+        $Pedido = "INSERT INTO `base de datos alumnos`(`Nombre`, `Edad`, `Email`, `Telefono`, `Fecha de registro`) VALUES ('$name','$age','$email','$tel','$FechaRegistro')";
+        $Resultado = mysqli_query($conex, $Pedido);
+        if ($Resultado) {
+            echo "<h>Te inscribiste a alumnos</h>";
+        } else {
+            echo "<h2>Ocurrio un error</h2>";
+        }
+    }
+}
